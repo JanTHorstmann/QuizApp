ede77990ce56534fb6c2a7f8626bad7192e05b8b
JanTHorstmann <janhorstmann@yahoo.de>
2023-05-20 12:58:45 +0200

    added quizcard and js

---
+++ js-data/array.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": "",
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": "",
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": "",
+    },
+
+    {
+        "question": "",
+        "answer_1": "",
+        "answer_2": "",
+        "answer_3": "",
+        "answer_4": "",
+        "right_answer": "",
+    },
+];
